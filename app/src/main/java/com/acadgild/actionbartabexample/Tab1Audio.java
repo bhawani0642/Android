d4684c5b1ed38984df3ec3c3b20f370dd2481c0e
bhawani0642 <bhawani0642@gmail.com>
2017-09-04 14:28:02 +0530

    Android_Assignment

---
+++ app/src/main/java/com/acadgild/actionbartabexample/Tab1Audio.java
@@ -0,0 +1,21 @@
+package com.acadgild.actionbartabexample;
+
+/**
+ * Created by Pri on 9/4/2017.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab1Audio extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1audio, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/acadgild/actionbartabexample/Tab2Video.java
@@ -0,0 +1,22 @@
+package com.acadgild.actionbartabexample;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Pri on 9/4/2017.
+ */
+
+public class Tab2Video extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1video, container, false);
+
+        return rootView;
+    }
+}
